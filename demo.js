dd1d9139d32f80e2d6e766908ed8fbf9d5dabf63
github username <github email address>
2019-10-14 17:02:24 -0400

    Done.

--- demo.js
+++ demo.js
@@ -1,11 +1,13 @@
 // We use this code, known as Objects, to control toggling like / unlike status
 
-let glyphStates = {
+let glyphStates =
+{
   "♡": "♥",
   "♥": "♡"
 };
 
-let colorStates = {
+let colorStates =
+{
   "red" : "",
   "": "red"
 };
@@ -14,22 +16,25 @@ let colorStates = {
 // Without JavaScript, clicking on these heart shapes does nothing. Uncomment
 // this code and refresh the demo page.
 
-// let articleHearts = document.querySelectorAll(".like");
+let articleHearts = document.querySelectorAll(".like");
 
-function likeCallback(e) {
+function likeCallback(e)
+{
   let heart = e.target;
   mimicServerCall()
-    .then(function(serverMessage){
+    .then(function(serverMessage)
+    {
       // STEP 2: Uncomment the next 3 lines.
       // We'll use Pillar 1 (DOM Manipulation) to update the screen and
       // mimic Pillar 3 (Server Communication) to only update the screen if the
       // sending of information to the server succeeds.
       alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
     })
-    .catch(function(error) {
+    .catch(function(error)
+    {
       alert("Something went wrong!");
     });
 }
@@ -39,11 +44,11 @@ function likeCallback(e) {
 // code when an "event" is fired. That's Pillar 2, event handling. Uncomment
 // this code.
 
-//for (let glyph of articleHearts) {
-  //glyph.addEventListener("click", likeCallback);
-//}
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
-// STEP 4: 
+// STEP 4:
 
 // When all the STEPs' code changes have been complete, you'll be able to see a
 // working demonstration of our reference example. Sure, it's maybe not as
@@ -54,9 +59,12 @@ function likeCallback(e) {
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall()
+{
+  return new Promise(function(resolve, reject)
+  {
+    setTimeout(function()
+    {
       resolve("Pretend remote server notified of action!");
     }, 300);
   });
