39a39f88ab6ea73a3a464fa0cd935f9d98cd0c61
mmusah <mmusah1@gmail.com>
2020-07-27 17:35:24 -0400

    Done.

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-// We use this code, known as Objects, to control toggling like / unlike status
+//We use this code, known as Objects, to control toggling like / unlike status
 
 let glyphStates = {
   "♡": "♥",
@@ -10,9 +10,9 @@ let colorStates = {
   "": "red"
 };
 
-// STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
-// Without JavaScript, clicking on these heart shapes does nothing. Uncomment
-// this code and refresh the demo page.
+//STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
+//Without JavaScript, clicking on these heart shapes does nothing. Uncomment
+//this code and refresh the demo page.
 
 let articleHearts = document.querySelectorAll(".like-glyph");
 
@@ -20,38 +20,38 @@ function likeCallback(e) {
   let heart = e.target;
   mimicServerCall()
     .then(function(serverMessage){
-      // STEP 2: Uncomment the next 3 lines.
-      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
-      // mimic Pillar 3 (Server Communication) to only update the screen if the
-      // sending of information to the server succeeds.
+      //STEP 2: Uncomment the next 3 lines.
+     // We'll use Pillar 1 (DOM Manipulation) to update the screen and
+      //mimic Pillar 3 (Server Communication) to only update the screen if the
+      //sending of information to the server succeeds.
       alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
       alert("Something went wrong!");
     });
 }
 
-// STEP 3: In order for the call to the server and the update of the screen to
-// work, the elements we identify in STEP 1 need to be told to run that update
-// code when an "event" is fired. That's Pillar 2, event handling. Uncomment
-// this code.
+//STEP 3: In order for the call to the server and the update of the screen to
+//work, the elements we identify in STEP 1 need to be told to run that update
+//code when an "event" is fired. That's Pillar 2, event handling. Uncomment
+//this code.
 
-//for (let glyph of articleHearts) {
-  //glyph.addEventListener("click", likeCallback);
-//}
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
-// STEP 4: 
+//STEP 4: 
 
-// When all the STEPs' code changes have been complete, you'll be able to see a
-// working demonstration of our reference example. Sure, it's maybe not as
-// pretty as a professional site, but they're only different from our site in
-// style, not substance.
+//When all the STEPs' code changes have been complete, you'll be able to see a
+//working demonstration of our reference example. Sure, it's maybe not as
+//pretty as a professional site, but they're only different from our site in
+//style, not substance.
 
 //------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
+//Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
 function mimicServerCall() {
